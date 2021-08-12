e21c9d1a30c362cc76ad0d71bffe030a0b5d010c
Silvio Normey Gómez <silviogn@users.noreply.github.com>
2021-08-12 19:40:36 -0300

    update

--- src/main/java/AulaEstruturaDados.java
+++ src/main/java/AulaEstruturaDados.java
@@ -3,6 +3,14 @@ public class AulaEstruturaDados {
 
     public static void main(String[] args){
         System.out.println("Hello estrutura de dados");
+        Pessoa p = new Pessoa();
+        p.message();
+    }
+
+    static class Pessoa {
+        public void message(){
+            System.out.println("Bom dia, tudo bom?");
+        }
     }
 
 }
